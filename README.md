b197f4396f633e6f84f3914f59bb6841da6a495d
Hariharan Sivakumar <hariharan.sivakumar@moneysupermarket.com>
2020-06-08 00:19:52 +0100

    setup python package

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # JSONPathGenerator
 Python project to generate Json path notations
+
+
+
+
+
+# USAGE
+
+   js = JSONPathGenerator()
+
+   js.get_json_path(<json-line>,notation='<str>')
+
+   notation = "dot"
+
+   notation = "bracket"
+
+
+# Usage Examples
+
+    js = JSONPathGenerator()
+
+    with open('sample_json.json', 'r') as Json_file:
+        for line in Json_file:
+            json_line=json.loads(line)
+            json_path_list=js.get_json_path(json_line,notation='dot')
+            print(json_path_list)
+
+
--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/guides/distributing-packages-using-setuptools/
-https://github.com/pypa/sampleproject
-"""
 
-# Always prefer setuptools over distutils
+
 from setuptools import setup, find_packages
 from os import path
 
@@ -17,20 +12,17 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
 
-    name='NestedJsonPath', 
+    name='json-path', 
 
     version='0.0.1',  
 
     description='Get Json path from a nested json document. specify notation dot or bracket',  
 
-
     long_description=long_description, 
 
     long_description_content_type='text/markdown', 
 
-
-    url='https://github.com/pypa/sampleproject',  
-
+    url='https://github.com/uniquetech/JSONPathGenerator.git',  
 
     author='hariharan sivakumar',  
 
@@ -55,78 +47,11 @@ setup(
     ],
 
 
-    keywords='json jsonpath xpath jsonnotation json parser safe parser',  
+    keywords='Json Jsonpath xpath jsonnotation json parser safe parser',  
 
-    package_dir={'': 'src'},  # Optional
+    package_dir={'': 'src'},  
 
-    packages=find_packages(where='src'),  # Required
+    py_modules=["JSONPathGenerator.py"],
 
-    # Specify which Python versions you support. In contrast to the
-    # 'Programming Language' classifiers above, 'pip install' will check this
-    # and refuse to install the project if the version does not match. See
-    # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires='>=3.5, <4',
-
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['peppercorn'],  # Optional
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). Users will be able to install these using the "extras"
-    # syntax, for example:
-    #
-    #   $ pip install sampleproject[dev]
-    #
-    # Similar to `install_requires` above, these must be valid existing
-    # projects.
-    extras_require={  # Optional
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.
-    package_data={  # Optional
-        'sample': ['package_data.dat'],
-    },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/distutils/setupscript.html#installing-additional-files
-    #
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('my_data', ['data/data_file'])],  # Optional
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    entry_points={  # Optional
-        'console_scripts': [
-            'sample=sample:main',
-        ],
-    },
-
-    # List additional URLs that are relevant to your project as a dict.
-    #
-    # This field corresponds to the "Project-URL" metadata fields:
-    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
-    #
-    # Examples listed include a pattern for specifying where the package tracks
-    # issues, where the source is hosted, where to say thanks to the package
-    # maintainers, and where to support the project financially. The key is
-    # what's used to render the link text on PyPI.
-    project_urls={  
-        'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
-        'Funding': 'https://donate.pypi.org',
-        'Say Thanks!': 'http://saythanks.io/to/example',
-        'Source': 'https://github.com/pypa/sampleproject/',
-    },
+    python_requires='>=3.5, <4'
 )
